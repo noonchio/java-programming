79258c6e16f394bbd03d3508e1e0fa0788319457
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-04 18:46:04 -0500

    add Jukebox1

---
+++ src/Streaming.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Streaming {
+    public static void main(String[] args) {
+
+
+        List<String> strings = List.of("I", "am", "a", "list", "of", "Strings");
+        Stream<String> stream = strings.stream();
+
+//        Stream<String> limit = stream.limit(4);
+//        long result = limit.count();
+//        System.out.println("result = " + result);
+
+//        List<String> result = limit.collect(Collectors.toList());
+//        System.out.println("result = " + result);
+
+        List<String> result = strings.stream()
+                .limit(4).toList();
+
+        System.out.println("result = " + result);
+
+    }
+}
---
+++ src/jukebox/Jukebox1.java
@@ -0,0 +1,32 @@
+package jukebox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jukebox1 {
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    public void go () {
+        List<String> songList = MockSongs.getSongStrings();
+        System.out.println(songList);
+    }
+}
+
+// mock code a stand in for the actual I/O code that the other programmer will provide later
+
+class MockSongs {
+
+    public static List<String> getSongStrings() {
+
+        List<String> songs = new ArrayList<>();
+        songs.add("somersault");
+        songs.add("cassidy");
+        songs.add("$10");
+        songs.add("havana");
+        songs.add("Cassidy");
+        songs.add("50 Ways");
+        return songs;
+    }
+}
