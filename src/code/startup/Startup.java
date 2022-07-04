13dd6c5956f1b0e7aefc6974835833f90ef3ab9e
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-04 14:24:23 -0500

    add Startup.java battleship inspired game

---
+++ src/code/startup/Startup.java
@@ -0,0 +1,35 @@
+package code.startup;
+
+import java.util.ArrayList;
+
+public class Startup {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> locs){
+        locationCells = locs;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "Destroyed";
+                System.out.println("Ouch! You sunk " + name + " : (" );
+            } else {
+                result = "hit";
+            }
+        }
+
+        return result;
+    }
+
+}
