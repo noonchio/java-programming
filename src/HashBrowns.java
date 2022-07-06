05410a7d6b9154b95dec07264539d7cb148a44f9
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-06 17:53:23 -0500

    add Hashset tutorial

---
+++ src/HashBrowns.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashBrowns {
+    public static void main(String[] args) {
+
+//        HashSet<String> h = new HashSet<String>();
+//        h.add("lemur");
+//        h.add("orangutan");
+//        h.add("spider monkey");
+//        h.add("silver-back gorilla");
+//
+//        h.remove("orangutan");
+////        h.clear removes everything
+////        h.size
+//        System.out.println(h.contains("spider monkey"));
+//
+//        System.out.println(h.size());
+
+        HashSet<Integer> hashbrowns = new HashSet<Integer>();
+
+        hashbrowns.add(13);
+        hashbrowns.add(24);
+        hashbrowns.add(5);
+
+        Iterator<Integer> it = hashbrowns.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        Object [] h = hashbrowns.toArray();
+        System.out.println();
+    }
+}
+
+// tutorial bhy Alex Lee @ https://www.youtube.com/watch?v=PeFyhRr42ac
