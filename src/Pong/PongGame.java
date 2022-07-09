a9dece58b9984ff2ce143c0f4611748feffe2831
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-08 20:49:35 -0500

    feat: add Main.java

--- src/Pong/PongGame.java
+++ src/Pong/PongGame.java
@@ -1,10 +1,10 @@
-package Pong;
-import java.awt
-
-
-
-public class PongGame {
-    public static void main(String[] args) {
-
-    }
-}
+//package Pong;
+//import java.awt
+//
+//
+//
+//public class PongGame {
+//    public static void main(String[] args) {
+//
+//    }
+//}
