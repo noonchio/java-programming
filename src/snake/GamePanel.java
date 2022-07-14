23124f54a10ec74dfc4fe13e8103d882eb405e80
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-14 16:25:34 -0500

    refactor: SnakeGame

--- src/snake/GamePanel.java
+++ src/snake/GamePanel.java
@@ -13,8 +13,8 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
-    final int x[] = new int [GAME_UNITS];
-    final int y[] = new int [GAME_UNITS];
+    final int[] x = new int [GAME_UNITS];
+    final int[] y = new int [GAME_UNITS];
     int bodyParts = 6;
     int applesEaten;
     int appleX;
@@ -50,15 +50,56 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g){
+        for(int i = 0; i <SCREEN_HEIGHT/UNIT_SIZE; i++) {
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+
+        //draw apple
+        g.setColor(Color.pink);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE );
+
+        //draw snake
+        for(int i =0; i < bodyParts; i++) {
+            if(i == 0) {
+                g.setColor(Color.YELLOW);
+                g.fillRect(x[i],y[i],UNIT_SIZE, UNIT_SIZE);
+            } else{
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i],y[i],UNIT_SIZE, UNIT_SIZE);
+
+            }
+        }
 
     }
 
     public void newApple(){
-
+            //generate coordinates
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))* UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))* UNIT_SIZE;
     }
 
     public void move() {
+            for(int i = bodyParts; i>0; i--) {
+                x[i] = x[i-1];
+                y[i] = y[i-1];
 
+            }
+
+            switch(direction) {
+                case 'U' :
+                    y[0] = y[0] - UNIT_SIZE;
+                    break;
+                case 'D' :
+                    y[0] = y[0] + UNIT_SIZE;
+                    break;
+                case 'L' :
+                    x[0] = x[0] - UNIT_SIZE;
+                    break;
+                case 'R' :
+                    x[0] = x[0] + UNIT_SIZE;
+                    break;
+            }
     }
 
     public void checkApple(){
