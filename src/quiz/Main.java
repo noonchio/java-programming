c73546f1623f52d5ffc4ffb669deb8da45dada68
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-15 15:54:10 -0500

    add Quiz.java

---
+++ src/quiz/Main.java
@@ -0,0 +1,8 @@
+package quiz;
+
+public class Main {
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz ();
+
+    }
+}
---
+++ src/quiz/Quiz.java
@@ -0,0 +1,21 @@
+package quiz;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Quiz implements ActionListener {
+
+    public Quiz(){
+
+    }
+
+    public void nextQuestion() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
