a4b2fa5d34c22cbc39f3445c079e1d5686ae8d5e
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-07-18 10:51:40 -0500

    add Quiz.java and add Main.java

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -6,8 +6,118 @@ import javax.swing.*;
 
 public class Quiz implements ActionListener {
 
-    public Quiz(){
+    String[] questions = {
+            "Which company created Java?",
+            "Which year was Java created?",
+            "What was Java originally called",
+            "Who is credited with creating Java?"
+    };
 
+    String[][] options = {
+            {"Sun Microsystems", "Starbucks", "Microsoft", "Alphabet"},
+            {"1089", "1996", "1972", "1980"},
+            {"Apple", "Latte", "Oak", "Koffing"},
+            {"Steve Jobs", "Bill Gates", "Jasmes Gosling", "Thor"}
+
+    };
+
+    char[] answers = {
+            'A',
+            'B',
+            'C',
+            'C'
+    };
+
+    char guess;
+    char answer;
+    int index;
+    int correct_guesses = 0;
+    int total_questions = questions.length;
+    int result;
+    int seconds = 10;
+
+    JFrame frame = new JFrame();
+    JTextField textfield = new JTextField();
+    JTextArea   textArea = new JTextArea();
+    JButton buttonA = new JButton();
+    JButton buttonB = new JButton();
+    JButton buttonC = new JButton();
+    JButton buttonD = new JButton();
+    JLabel answer_labelA = new JLabel();
+    JLabel answer_labelB = new JLabel();
+    JLabel answer_labelC = new JLabel();
+    JLabel answer_labelD = new JLabel();
+    JLabel time_label = new JLabel();
+    JLabel seconds_left = new JLabel();
+    JTextField number_right = new JTextField();
+    JTextField percentage = new JTextField();
+
+
+
+    public Quiz(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(550,550);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(null);
+        frame.setResizable(false);
+
+        textfield.setBounds(0,0,550,50);
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free",Font.BOLD, 20));
+        textfield.setBorder(BorderFactory.createBevelBorder(1));
+        textfield.setHorizontalAlignment(JTextField.CENTER);
+        textfield.setEditable(false);
+
+
+        textArea.setBounds(0,50,550,50);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBackground(new Color(25,25,25));
+        textArea.setForeground(new Color(25,255,0));
+        textArea.setFont(new Font("Ink Free",Font.BOLD, 20));
+        textArea.setBorder(BorderFactory.createBevelBorder(1));
+        textArea.setEditable(false);
+
+        buttonA.setBounds(0, 100, 100, 100);
+        buttonA.setFont(new Font("Ink Free", Font.BOLD,35));
+        buttonA.setFocusable(false);
+        buttonA.addActionListener(this);
+        buttonA.setText("A");
+        buttonA.setBackground(Color.black);
+
+        buttonB.setBounds(0, 200, 100, 100);
+        buttonB.setFont(new Font("Ink Free", Font.BOLD,35));
+        buttonB.setFocusable(false);
+        buttonB.addActionListener(this);
+        buttonB.setText("B");
+        buttonB.setBackground(Color.black);
+
+        buttonC.setBounds(0, 300, 100, 100);
+        buttonC.setFont(new Font("Ink Free", Font.BOLD,35));
+        buttonC.setFocusable(false);
+        buttonC.addActionListener(this);
+        buttonC.setText("C");
+        buttonC.setBackground(Color.black);
+
+        buttonD.setBounds(0, 400, 100, 100);
+        buttonD.setFont(new Font("Ink Free", Font.BOLD,35));
+        buttonD.setFocusable(false);
+        buttonD.addActionListener(this);
+        buttonD.setText("D");
+        buttonD.setBackground(Color.black);
+
+
+
+
+
+        frame.add(buttonA);
+        frame.add(buttonB);
+        frame.add(buttonC);
+        frame.add(buttonD);
+        frame.add(textArea);
+        frame.add(textfield);
+        frame.setVisible(true);
     }
 
     public void nextQuestion() {
@@ -16,6 +126,14 @@ public class Quiz implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void displayAnswer(){
+
+    }
+
+    public void results(){
 
     }
 }
